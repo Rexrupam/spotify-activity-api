c6ea839460977cd27bca16ea816047bfa980463a
rupam <rexrupam70@gmail.com>
2025-04-05 20:02:37 +0530

    add api to get only subscriber's name and email

--- src/controller/subscriber.controller.js
+++ src/controller/subscriber.controller.js
@@ -15,3 +15,12 @@ export const getSubscriber=async(req,res)=>{
       return res.status(500).send('Internal server error')
    }
 }
+export const getSubscriberName=async(req,res)=>{
+    try {
+        const subscribersNameandEmail = await Subscriber.find().select('-_id name subscribedChannel')
+        return res.status(200).json({message: "Subscriber's name and email fetched successfully",subscribersNameandEmail})
+    } catch (error) {
+        console.log("Something went wrong", error)
+        return res.status(500).send("Internal server error")
+    }
+}
--- src/router/subscriber.router.js
+++ src/router/subscriber.router.js
@@ -1,9 +1,10 @@
-import { dataFile, getSubscriber,  } from "../controller/subscriber.controller.js";
+import { dataFile, getSubscriber, getSubscriberName  } from "../controller/subscriber.controller.js";
 import {Router} from "express"
 
 const router = Router();
 
 router.use(dataFile)   // apply data file to all routes
 router.route('/subscribers').get(getSubscriber)
+router.route('/subscribers/names').get(getSubscriberName)
 
 export default router;
